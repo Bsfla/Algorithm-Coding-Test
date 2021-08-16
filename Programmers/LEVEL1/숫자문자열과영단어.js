c2cd725811c5ea82d792e2d9d0340bf949aa40cd
Bsfla <baebeomsu77@gmail.com>
2021-08-16 19:18:47 +0900

    문제풀이

---
+++ Programmers/LEVEL1/숫자문자열과영단어.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    const word = ['zero', 'one', 'two', 'three','four','five','six','seven','eight','nine'];
+    
+    let str = '';
+    let result = '';
+   
+    s.split("").forEach((el) => {
+       if (isNaN(el)) {
+           str += el;
+           if (word.includes(str)) {
+               result += word.indexOf(str);
+               str = '';
+           }
+       }
+       else result += el;
+    })
+    
+    return Number(result);
+}
