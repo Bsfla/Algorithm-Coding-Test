c602e328e4292136eef681410f802768081e3a4e
Bsfla <baebeomsu77@gmail.com>
2021-10-04 20:43:42 +0900

    commit

--- BaekJoon/스택/큐/스택.js
+++ BaekJoon/스택/큐/스택.js
@@ -14,19 +14,22 @@ rl.on("line", (line) => {
   let stack = [];
   let result = [];
 
+  for (let i = 0; i < input.length; i++) {
+    M[i] = input[i].split(" ");
+  }
+
   for (let i = 0; i < N; i++) {
-      M[i] = input[i].split(" ");
+    if (M[i][0] === "push") stack.push(M[i][1]);
+    else if (M[i][0] === "top") {
+      stack.length ? result.push(stack[stack.length - 1]) : result.push(-1);
+    } else if (M[i][0] === "pop") {
+      stack.length ? result.push(stack.pop()) : result.push(-1);
+    } else if (M[i][0] === "size") {
+      result.push(stack.length);
+    } else if (M[i][0] === "empty") {
+      !stack.length ? result.push(1) : result.push(0);
+    }
   }
 
-  for (let i = 0; i < M.length; i++) {
-      if (M[i][0] === 'push') stack.push(M[i][1]);
-      else if (M[i][0] === 'pop') stack.length === 0 ? result.push(-1) : result.push(stack.pop());
-      else if (M[i][0] === 'size') result.push(stack.length);
-      else if (M[i][0] === 'empty') stack.length === 0 ? result.push(1) : result.push(0);
-      else stack.length ? result.push(stack[stack.length - 1]) : result.push(-1);
-  }
-
-  console.log(result.join('\n'));
-
-
+  console.log(result.join("\n"));
 });
