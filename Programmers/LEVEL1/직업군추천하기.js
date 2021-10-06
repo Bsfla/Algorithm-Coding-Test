9fd62b860f80d302f9ae1c0bedb04872a524b872
Bsfla <baebeomsu77@gmail.com>
2021-10-06 23:04:17 +0900

    commit

--- Programmers/LEVEL1/직업군추천하기.js
+++ Programmers/LEVEL1/직업군추천하기.js
@@ -1,6 +1,6 @@
 function solution(table, languages, preference) {
   const array = table.map((el) => el.split(" "));
-  const real = {};
+  const preferLanguages = {};
   const point = ["직업군", 5, 4, 3, 2, 1];
   const job = ["SI", "CONTENTS", "HARDWARE", "PORTAL", "GAME"];
   const total = [];
@@ -11,13 +11,15 @@ function solution(table, languages, preference) {
 
   for (let i = 0; i < languages.length; i++) {
     let key = languages[i];
-    real[key] = preference[i];
+    preferLanguages[key] = preference[i];
   }
 
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < languages.length; j++) {
       if (array[i].indexOf(languages[j]) !== -1) {
-        result.push(real[languages[j]] * point[array[i].indexOf(languages[j])]);
+        result.push(
+          preferLanguages[languages[j]] * point[array[i].indexOf(languages[j])]
+        );
       }
     }
     total.push(result.reduce((acc, bcc) => acc + bcc, 0));
