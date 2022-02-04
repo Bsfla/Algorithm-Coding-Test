c74bdef124f4f0bb58c6390d9bc4b6a69038d416
baebumsoo <baebeomsu77@gmail.com>
2022-02-04 16:33:50 +0900

    commit

--- Programmers/LEVEL1/비밀지도.js
+++ Programmers/LEVEL1/비밀지도.js
@@ -1,29 +1,53 @@
 function solution(n, arr1, arr2) {
-    const answer = [];
-    const result = [];
-    
-    let sum = 0;
-    let str = '';
-    
-    for (let i = 0; i < arr1.length; i++) {
-        sum = arr1[i] | arr2[i];
-        sum = sum.toString(2);
-        answer.push(sum)
+  const answer = [];
+  const result = [];
+
+  let sum = 0;
+  let str = "";
+
+  for (let i = 0; i < arr1.length; i++) {
+    sum = arr1[i] | arr2[i];
+    sum = sum.toString(2);
+    answer.push(sum);
+  }
+
+  for (let i = 0; i < answer.length; i++) {
+    for (let j = 0; j < answer.length; j++) {
+      if (answer[i][j] === "1") str += "#";
+      else if (answer[i][j] === "0") str += " ";
     }
-    
-    for (let i = 0; i < answer.length; i++) {
-        for (let j = 0; j < answer.length; j++) {
-            if (answer[i][j] === '1') str += '#';
-            else if(answer[i][j] === '0') str += ' ';
-        }
-       
-        if (answer[i].length !== n) {
-           for (let k = 0; k < n - answer[i].length; k++) {
-                str = ' ' + str;
-            }
-        }
-        result.push(str);
-        str = ''
+
+    if (answer[i].length !== n) {
+      for (let k = 0; k < n - answer[i].length; k++) {
+        str = " " + str;
+      }
     }
-   return result;
+    result.push(str);
+    str = "";
+  }
+  return result;
+}
+//
+
+function solution(n, arr1, arr2) {
+  const result = arr1.map((el, idx) => {
+    return (el | arr2[idx]).toString(2);
+  });
+  let str = "";
+
+  return result.map((el, idx) => {
+    str = el;
+    if (el.length !== n) {
+      for (let i = 0; i < n - el.length; i++) {
+        str = "0" + str;
+      }
+    }
+    return str
+      .split("")
+      .map((num) => {
+        if (num === "0") return (num = " ");
+        else if (num === "1") return (num = "#");
+      })
+      .join("");
+  });
 }
