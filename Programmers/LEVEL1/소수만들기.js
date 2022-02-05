b59b65cc6a1a03b725e94f111906718dd2b074ac
baebumsoo <baebeomsu77@gmail.com>
2022-02-05 23:52:53 +0900

    commit

---
+++ Programmers/LEVEL1/소수만들기.js
@@ -0,0 +1,24 @@
+function isPrime(num) {
+  if (num === 2) return true;
+
+  for (let i = 2; i < num; i++) {
+    if (num % 1 === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let result = 0;
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) result += 1;
+      }
+    }
+  }
+
+  return result;
+}
---
+++ Programmers/LEVEL1/최소직사각형.js
@@ -0,0 +1,33 @@
+function solution(sizes) {
+    const w = sizes.map(el => el[0]);
+    const h = sizes.map(el => el[1]);
+    let tmp = 0;
+     
+     for (let i = 0; i < sizes.length; i++) {
+         if (w[i] < h[i]) {
+             tmp = w[i];
+             w[i] = h[i];
+             h[i] = tmp;
+         }
+     }
+     
+     return Math.max(...w) * Math.max(...h);
+    
+    
+ }
+
+ function solution(sizes) {
+    const arr = sizes.map(el => {
+       return el.sort((a, b) => b - a)
+    })
+    
+    const w = Math.max(...arr.map(el => {
+        return el[0];
+    }));
+     const h = Math.max(...arr.map(el => {
+        return el[1];
+    }));
+    
+    return w * h;
+    
+ }
