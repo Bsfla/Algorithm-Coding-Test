e00a7739e7ca73e9389a433a224c3fe25fcfb0e6
baebumsoo <baebeomsu77@gmail.com>
2022-10-07 18:47:48 +0900

    commit

--- Programmers/LEVEL1/성격유형검사.js
+++ Programmers/LEVEL1/성격유형검사.js
@@ -50,3 +50,39 @@ function solution(survey, choices) {
 
   return handleCompareScore(typeScores, personalityType);
 }
+
+//2
+
+function handleCompareScore(personalityType) {
+  const typePoints = Object.entries(personalityType);
+  let answer = "";
+
+  for (let i = 0; i < typePoints.length; i += 2) {
+    const [curType, curPoint] = typePoints[i];
+    const [nextType, nextPoint] = typePoints[i + 1];
+
+    if (curPoint < nextPoint) answer += nextType;
+    else answer += curType;
+  }
+
+  return answer;
+}
+
+function handleAddScore(choice) {
+  const score = { 1: 3, 2: 2, 3: 1, 4: 0, 5: 1, 6: 2, 7: 3 };
+
+  return score[choice];
+}
+
+function solution(survey, choices) {
+  const typeScoreBoard = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+
+  for (let i = 0; i < survey.length; i++) {
+    if (choices[i] > 0 && choices[i] < 4)
+      typeScoreBoard[survey[i][0]] += handleAddScore(choices[i]);
+    else if (choices[i] > 4 && choices[i] < 8)
+      typeScoreBoard[survey[i][1]] += handleAddScore(choices[i]);
+  }
+
+  return handleCompareScore(typeScoreBoard);
+}
