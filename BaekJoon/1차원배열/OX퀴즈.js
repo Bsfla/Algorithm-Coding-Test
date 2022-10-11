a3c3c539988a8db0d7afe1ad8a310681a6b96d71
baebumsoo <baebeomsu77@gmail.com>
2022-10-12 01:41:54 +0900

    commit

--- BaekJoon/1차원배열/OX퀴즈.js
+++ BaekJoon/1차원배열/OX퀴즈.js
@@ -1,30 +1,30 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function(line) {
-    input.push(line);
-}).on('close', function() {
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
   const num = Number(input[0]);
 
-  let count = 0,
-      sum = 0;
-  
+  let count = 0;
+  let sum = 0;
+
   for (let i = 1; i < num; i++) {
-      count = 1;
-      sum = 0;
-    for (let j = 0; j < input[i].length; j++) {
-         if (input[i][j] === 'O') {
-             sum += count;
-             count++;
-        } else count = 1;
+    count = 1;
+    sum = 0;
+    for (let j = 1; j < input[i].length; j++) {
+      if (input[i][j] === "O") {
+        sum += count;
+        count++;
+      } else count = 1;
     }
     console.log(sum);
   }
-  
+
   process.exit();
 });
