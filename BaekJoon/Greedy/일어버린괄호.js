ec060ab53369f317de3cf14d5121c9e682ec7254
baebumsoo <baebeomsu77@gmail.com>
2022-10-29 15:14:43 +0900

    commit

--- BaekJoon/Greedy/일어버린괄호.js
+++ BaekJoon/Greedy/일어버린괄호.js
@@ -8,8 +8,20 @@ let input = 0;
 rl.on("line", function (line) {
   input = line;
 }).on("close", function () {
-  const calculation = input.map(el => {
-      el.split('-')
-  })
+  const calculate = input.split("-").map((str) =>
+    str
+      .split("+")
+      .map(Number)
+      .reduce((acc, curr) => {
+        return acc + curr;
+      }, 0)
+  );
+  let result = calculate[0];
+
+  for (let i = 1; i < calculate.length; i++) {
+    result -= calculate[i];
+  }
+
+  console.log(result);
   process.exit();
 });
--- BaekJoon/Greedy/회의실배정.js
+++ BaekJoon/Greedy/회의실배정.js
@@ -9,24 +9,20 @@ const input = [];
 rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
-  let N = parseInt(input.shift());
-  let timeList = [];
+  const N = input.shift();
+  const meetings = input
+    .map((el) => el.split(" ").map(Number))
+    .sort((a, b) => {
+      if (a[1] == b[1]) return a[0] - b[0];
+      return a[1] - b[1];
+    });
   let count = 0;
-  let time = 0;
+  let startTime = 0;
 
-  for (let i = 0; i < input.length; i++) {
-    timeList[i] = input[i].split(" ").map(Number);
-  }
-
-  timeList.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    return a[1] - b[1];
-  });
-
-  timeList.map((el) => {
-    if (time <= el[0]) {
+  meetings.forEach((meeting) => {
+    if (meeting[0] >= startTime) {
+      startTime = meeting[1];
       count += 1;
-      time = el[1];
     }
   });
 
