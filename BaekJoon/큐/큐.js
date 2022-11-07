cc44ef3346cd4d0b0e9935b9ae414eabbff9318c
baebumsoo <baebeomsu77@gmail.com>
2022-11-07 18:58:48 +0900

    commit

--- BaekJoon/큐/큐.js
+++ BaekJoon/큐/큐.js
@@ -9,23 +9,23 @@ const input = [];
 rl.on("line", (line) => {
   input.push(line);
 }).on("close", () => {
-  const N = parseInt(input.shift());
-  const M = input.map((el) => {
-    return el.split(" ");
-  });
+  const N = input.shift();
   const que = [];
   const result = [];
 
-  for (let i = 0; i < M.length; i++) {
-      if (M[i][0] === 'push') que.push(parseInt(M[i][1]));
-      else if (M[i][0] === 'pop') que.length ? result.push(que.shift()) : result.push(-1);
-      else if (M[i][0] === 'empty') que.length ? result.push(0) : result.push(1);
-      else if (M[i][0] === 'front') que.length ? result.push(que[0]) : result.push(-1);
-      else if (M[i][0] === 'back') que.length ? result.push(que[que.length - 1]) : result.push(-1);
-      else if (M[i][0] === 'size') result.push(que.length);
-      
-  }
-  
+  input.forEach((el) => {
+    const command = el.split(" ");
+    if (command[0] === "push") que.push(parseInt(command[1]));
+    else if (command[0] === "pop")
+      que.length ? result.push(que.shift()) : result.push(-1);
+    else if (command[0] === "size") result.push(que.length);
+    else if (command[0] === "empty")
+      que.length ? result.push(0) : result.push(1);
+    else if (command[0] === "front")
+      que.length ? result.push(que[0]) : result.push(-1);
+    else if (command[0] === "back")
+      que.length ? result.push(que[que.length - 1]) : result.push(-1);
+  });
 
-  console.log(result.join("\n"));   
+  console.log(result.join("\n"));
 });
