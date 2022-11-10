594471907963b16d08bee026bf9e586e562382e1
baebumsoo <baebeomsu77@gmail.com>
2022-11-10 17:24:12 +0900

    commit

--- Programmers/LEVEL2/기능개발.js
+++ Programmers/LEVEL2/기능개발.js
@@ -1,23 +1,50 @@
 function solution(progresses, speeds) {
-    const answers = [];
-    let cnt = 0;
-    
-   while(speeds.length > 0) {
-       for (let i = 0; i < speeds.length; i++) {
-           if (progresses[i] < 100) progresses[i] += speeds[i]; 
-       }
-       
-       while(progresses[0] >= 100) {
-           cnt++;
-           progresses.shift();
-           speeds.shift();
-       }
-       
-       if (cnt > 0) {
-           answers.push(cnt);
-           cnt = 0;
-       }
-   }
-    
-    return answers;
+  const answers = [];
+  let cnt = 0;
+
+  while (speeds.length > 0) {
+    for (let i = 0; i < speeds.length; i++) {
+      if (progresses[i] < 100) progresses[i] += speeds[i];
+    }
+
+    while (progresses[0] >= 100) {
+      cnt++;
+      progresses.shift();
+      speeds.shift();
+    }
+
+    if (cnt > 0) {
+      answers.push(cnt);
+      cnt = 0;
+    }
+  }
+
+  return answers;
+}
+
+function solution(progresses, speeds) {
+  const releaseDays = [];
+  const skillCounts = [];
+
+  for (let i = 0; i < progresses.length; i++) {
+    let count = 0;
+
+    while (progresses[i] < 100) {
+      progresses[i] += speeds[i];
+      count += 1;
+
+      if (progresses[i] >= 100) {
+        if (releaseDays[releaseDays.length - 1] >= count) {
+          let frontSkillCount = skillCounts.pop();
+          skillCounts.push(frontSkillCount + 1);
+        } else {
+          releaseDays.push(count);
+          skillCounts.push(1);
+        }
+
+        break;
+      }
+    }
+  }
+  return skillCounts;
 }
--- Programmers/LEVEL2/프린터.js
+++ Programmers/LEVEL2/프린터.js
@@ -1,16 +1,16 @@
 function solution(priorities, location) {
-    const que = [];
-    let firstNum = 0;
-    let count = 0;
-    
-    while (priorities.length > 0) {
-        firstNum = priorities.shift();
-        if (firstNum < Math.max(...priorities)) priorities.push(firstNum);
-       else {
-           count += 1;
-           if (location === 0) return count;
-       }
-        location -= 1;
-        if (location === -1) location = priorities.length - 1;
-   }
+  let count = 0;
+
+  while (priorities.length !== 0) {
+    const docs = priorities.shift();
+    const max = Math.max(...priorities);
+
+    if (max > docs) priorities.push(docs);
+    else {
+      count++;
+      if (location === 0) return count;
+    }
+    location--;
+    if (location === -1) location = priorities.length - 1;
+  }
 }
