7eeaba0c6e4769fc0c4cb36e512b419af656ca86
baebumsoo <baebeomsu77@gmail.com>
2022-11-27 23:43:44 +0900

    commit

--- InflearnCodingTest/스택/후위식연산.js
+++ InflearnCodingTest/스택/후위식연산.js
@@ -1,32 +1,22 @@
-function solution(s) {
-    const stack = [];
-    let result = 0;
+function solution(value) {
+  const stack = [];
 
-    for (let x of s) {
-        if (!isNaN(x)) stack.push(parseInt(x));
-        else {
-            if (x === '+') {
-                result = stack[stack,length - 2] + stack[stack.length - 1];
-                stack.pop();
-                stack.pop();
-                stack.push(value);
-            } else if (x === '-') {
-                result = stack[stack,length - 2] - stack[stack.length - 1];
-                stack.pop();
-                stack.pop();
-                stack.push(value);
-            } else if (x === '*') {
-                result = stack[stack,length - 2] * stack[stack.length - 1];
-                stack.pop();
-                stack.pop();
-                stack.push(value);
-            } else if (x === '/') {
-                result = stack[stack,length - 2] / stack[stack.length - 1];
-                stack.pop();
-                stack.pop();
-                stack.push(value);
-            }
-        }
-    }
-    console.log(stack[0]);
+  value.split("").forEach((el) => {
+    if (isNaN(el)) {
+      let frontNum = stack.pop();
+      let secondNum = stack.pop();
+
+      if (el === "+") {
+        stack.push(frontNum + secondNum);
+      } else if (el === "-") {
+        stack.push(secondNum - frontNum);
+      } else if (el === "*") {
+        stack.push(secondNum * frontNum);
+      } else {
+        stack.push(secondNum / frontNum);
+      }
+    } else stack.push(Number(el));
+  });
+
+  console.log(stack[0]);
 }
