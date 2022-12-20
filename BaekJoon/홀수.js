7a845081aa4cdc005b8b2c3361729079e5e827de
baebumsoo <baebeomsu77@gmail.com>
2022-12-20 23:18:04 +0900

    commit

--- BaekJoon/홀수.js
+++ BaekJoon/홀수.js
@@ -1,13 +1,1 @@
-function a() {
-  const newOption = { ...cur };
-  if (e.target.name !== "option_price") {
-    newOption[e.target.name] = e.target.value;
-
-    return newOption;
-  }
-
-  if (e.target.value === 
-    newOption[e.target.nam    return newOp;
-  }
-  newOption[e.target.name] = parseIn;
-}
+ssds;
