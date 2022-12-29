d7bd9d5960b391e31808f56f1bf372656a31ab1b
baebumsoo <baebeomsu77@gmail.com>
2022-12-29 23:54:52 +0900

    commit

---
+++ Programmers/LEVEL2/행렬의곱셈.js
@@ -0,0 +1,21 @@
+function solution(arr1, arr2) {
+  const answer = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    const result = [];
+
+    for (let j = 0; j < arr2[0].length; j++) {
+      let sum = 0;
+
+      for (let k = 0; k < arr2.length; k++) {
+        sum += arr1[i][k] * arr2[k][j];
+      }
+
+      result.push(sum);
+    }
+
+    answer.push(result);
+  }
+
+  return answer;
+}
