d1d1763f748930431e9a0c3125377770be85887c
baebumsoo <baebeomsu77@gmail.com>
2023-02-26 23:06:24 +0900

    commit

--- Algorithm/sort/버블정렬.js
+++ Algorithm/sort/버블정렬.js
@@ -4,7 +4,7 @@ function bubbleSort(array) {
        for (let j = 0; j < array.length - 1 - i; j++) {
            if (array[j] > array[j + 1]) {
                swap = array[j];
-               array[j] = array[j + 1];
+               array[j] = array[j + 1
                array[j + 1] = swap;           
             }
        }
