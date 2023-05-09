3e734dca8dc1d9f88512d4bae2ed382e57c9d694
baebumsoo <baebeomsu77@gmail.com>
2023-05-09 23:57:59 +0900

    commit

--- InflearnCodingTest/브루트포스/K번째큰수.js
+++ InflearnCodingTest/브루트포스/K번째큰수.js
@@ -1,20 +1,18 @@
 function solution(k, arr) {
-    const answer = [];
-    let sum = 0;
-    
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            for (let k = j + 1; k < arr.length; k++) {
-                sum =   arr[i] + arr[j] + arr[k];
-                answer.push(sum);
-            }
-        }
-    }
+  const answer = [];
+  let sum = 0;
 
-    answer.sort((a,b) => b - a);
-    return answer[k - 1];
-    
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        sum = arr[i] + arr[j] + arr[k];
+        answer.push(sum);
+      }
+    }
+  }
+
+  return answer[k - 1];
 }
 
-let arr = [13,15,34,23,45,65,33,11,26,42];
-console.log(solution(3, arr))
+let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(3, arr));
